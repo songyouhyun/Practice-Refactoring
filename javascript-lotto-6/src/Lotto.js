62d4be5e02604756ea9bac6bfab66d2e177a8ac6
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-05-19 23:23:13 +0900

    예제 추가

---
+++ javascript-lotto-6/src/Lotto.js
@@ -0,0 +1,27 @@
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  #validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    const uniqueArray = [];
+    numbers.forEach((number) => {
+      if (uniqueArray.includes(number)) {
+        throw new Error("[ERROR] 중복되지 않는 숫자여야 합니다.")
+      }
+      uniqueArray.push(number);
+    });
+  }
+
+  get numbers() {
+    return this.#numbers;
+  }
+}
+
+export default Lotto;
