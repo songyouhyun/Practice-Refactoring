3ef2da651520d6ccdc6228df12b416bb427e592a
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-05-24 16:40:14 +0900

    refactor: 추상화 수준 맞추기

--- src/lotto/src/Lotto.ts
+++ src/lotto/src/Lotto.ts
@@ -6,17 +6,25 @@ class Lotto {
     this._numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-    const uniqueArray = [];
+  private validate(numbers): void {
+    this.validateLength6(numbers);
+    this.validateDuplicate(numbers);
+  }
+
+  private validateDuplicate(numbers) {
+    const uniqueArray: number[] = [];
     numbers.forEach((number) => {
       if (uniqueArray.includes(number)) {
         throw new Error("[ERROR] 중복되지 않는 숫자여야 합니다.")
       }
       uniqueArray.push(number);
     });
+  }
+
+  private validateLength6(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
   }
 
   get numbers() {
