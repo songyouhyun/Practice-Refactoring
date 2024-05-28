3891806a1ef285e418831d6f850647714c1d7ca8
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-05-28 19:27:32 +0900

    refactor(baseball/App): 테스트하기 어려운 랜덤값을 외부로 보내서 Computer를 테스트 가능한 코드로 변경

--- src/baseball/src/controller/App.ts
+++ src/baseball/src/controller/App.ts
@@ -2,8 +2,9 @@ import { Player } from "../model/Player";
 import { Score } from "../model/Score";
 import { InputView } from "../view/InputView";
 import { OutputView } from "../view/OutputView";
-import {Computer} from "../model/Computer";
 import {GameStatus} from "../model/GameStatus";
+import Random from "../../../utils/Random";
+import Computer from "../model/Computer";
 
 class App {
 
@@ -18,23 +19,27 @@ class App {
         this.outputView.printWelcomeMessage();
 
         do {
-            const computer: Computer = new Computer();
-            await this.playRound(computer.numbers);
+            const randoms: number[] = Random.pickUniqueNumbersInRange(1, 9, 3);
+            const computer: Computer = new Computer(randoms);
+            await this.playRound(computer);
         } while (!this.restartOrEnd.isGameEnd());
 
         this.outputView.printGoodbyeMessage();
     }
 
-    private async playRound(computer: number[]): Promise<void> {
+    private async playRound(computer: Computer): Promise<void> {
         const score: Score = new Score();
+
         do {
             const numbers: number[] = await this.inputView.getNumbers();
             const player: Player = new Player(numbers);
 
-            score.countStrikeOrBall(computer, player.numbers)
+            score.countStrikeOrBall(computer.numbers, player.numbers)
+
             const result: string = score.getResultOfScore();
             this.outputView.printResult(result);
         } while (!score.isRoundEnd());
+
         this.restartOrEnd = await this.inputView.getRestartOrEnd();
     }
 }
