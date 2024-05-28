d2c24617f671260dccd4e34f7c4c07a4c1246d1f
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-05-28 19:24:41 +0900

    refactor: jest.mock()을 이용한 모듈 모킹

--- src/baseball/__tests__/ApplicationTest.ts
+++ src/baseball/__tests__/ApplicationTest.ts
@@ -4,9 +4,11 @@ import App from "../src/controller/App";
 import {ConsoleInputView} from "../src/view/ConsoleInputView";
 import {ConsoleOutputView} from "../src/view/ConsoleOutputView";
 
+jest.mock('../../utils/Random');
+jest.mock('../../utils/Console');
+
 const mockQuestions = (inputs: string[]) => {
-  Console.readLineAsync = jest.fn();
-  const mockReadLineAsync = Console.readLineAsync as jest.Mock;
+  const mockReadLineAsync = <jest.Mock>Console.readLineAsync;
 
   mockReadLineAsync.mockImplementation(() => {
     const input = inputs.shift();
@@ -14,11 +16,11 @@ const mockQuestions = (inputs: string[]) => {
   });
 };
 
-const mockRandoms = (numbers: number[]): void => {
-  Random.pickNumberInRange = jest.fn();
-  numbers.reduce((acc, number) => {
-    return (acc as jest.Mock).mockReturnValueOnce(number);
-  }, Random.pickNumberInRange);
+const mockRandoms = (numbers: number[][]): void => {
+  const mockPickUniqueNumbersInRange = <jest.Mock>Random.pickUniqueNumbersInRange;
+  numbers.forEach((number) => {
+    mockPickUniqueNumbersInRange.mockReturnValueOnce(number);
+  })
 };
 
 const getLogSpy = () => {
@@ -30,9 +32,9 @@ const getLogSpy = () => {
 describe("숫자 야구 게임", (): void => {
   test("게임 종료 후 재시작", async (): Promise<void> => {
     // given
-    const randoms: number[] = [1, 3, 5, 5, 8, 9];
+    const randoms: number[][] = [[1, 3, 5], [5, 8, 9]];
     const answers: string[] = ["246", "135", "1", "597", "589", "2"];
-    const logSpy: jest.SpyInstance = getLogSpy();
+    const logSpy = getLogSpy();
     const messages: string[] = ["낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료"];
 
     mockRandoms(randoms);
@@ -50,10 +52,10 @@ describe("숫자 야구 게임", (): void => {
 
   test("예외 테스트", async () => {
     // given
-    const randoms = [1, 3, 5];
-    const answers = ["1234"];
+    const randoms: number[] = [1, 3, 5];
+    const answers: string[] = ["1234"];
 
-    mockRandoms(randoms);
+    mockRandoms([randoms]);
     mockQuestions(answers);
 
     const app: App = new App(new ConsoleInputView(), new ConsoleOutputView());
