162ffcdf052c3a5518cdd440b81a46df24ef32ef
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-05-29 16:47:43 +0900

    test: Console 단위 테스트 작성

---
+++ src/utils/__tests__/ConsoleTest.ts
@@ -0,0 +1,45 @@
+import readline from "readline";
+import Console from "../Console";
+
+describe('Console', () => {
+    describe('print', () => {
+        it('주어진 메시지를 콘솔에 출력해야 한다.', () => {
+            // given
+            const message = 'test';
+            const logSpy = jest.spyOn(console, "log");
+
+            // when
+            Console.print(message)
+
+            // then
+            expect(logSpy).toHaveBeenCalledWith(message);
+        });
+    });
+
+    describe('readLine', () => {
+        const query = "test";
+
+        it('사용자가 입력한 값을 반환해야 한다.', async () => {
+            // given
+            const userInput: string = "user input";
+            const createInterfaceMock = jest.spyOn(readline, "createInterface");
+            const readlineMock = {
+                question: jest.fn((query: string, callback: (input: string) => void) => {
+                    callback(userInput);
+                }),
+                close: jest.fn(),
+            };
+            createInterfaceMock.mockReturnValue(readlineMock as unknown as readline.Interface);
+
+            // when
+            const result: string = await Console.readLineAsync(query);
+
+            // then
+            expect(result).toBe(userInput);
+            expect(readlineMock.question).toHaveBeenCalledWith(query, expect.any(Function));
+            expect(readlineMock.close).toHaveBeenCalled();
+
+            createInterfaceMock.mockRestore();
+        });
+    });
+});
